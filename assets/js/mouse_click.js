3b1058f617ef7b7d9ea553a67d35911d247d1db6
kennyweichen <kennychen0713@gmail.com>
2025-08-06 14:13:24 -0500

    ad github repos

--- assets/js/mouse_click.js
+++ assets/js/mouse_click.js
@@ -1,4 +1,12 @@
 document.addEventListener('mousedown', function () {
+    document.documentElement.classList.add('clicking');
+  });
+  
+  document.addEventListener('mouseup', function () {
+    document.documentElement.classList.remove('clicking');
+  });
+
+  document.addEventListener('mousedown', function () {
     document.body.classList.add('clicking');
   });
   
